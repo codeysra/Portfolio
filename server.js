05869856e918afcd9a7c64cb6d5a226a0e03fa04
Yousra Saidani <yousaidani@gmail.com>
2018-06-27 09:08:29 -0400

    added server for production mode

---
+++ server.js
@@ -0,0 +1,15 @@
+const path= require('path');
+const express = require('express');
+const app = express();
+const publicPathDir = path.join(__dirname,'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPathDir));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(publicPathDir,'index.html'));
+});
+
+app.listen(port,()=>{
+    console.log("Server is up...")
+});
